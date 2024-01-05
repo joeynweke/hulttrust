21edb603f7744e76f8683d1028dcfa780863cfb4
joeynweke <97444149+joeynweke@users.noreply.github.com>
2024-01-05 13:55:26 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -41,7 +41,7 @@ let firstAmount = {
 } 
 
 localStorage.setItem('savings', firstAmount.first.toLocaleString())
-localStorage.removeItem('savings')
+//localStorage.removeItem('savings')
 
 
 
@@ -99,6 +99,7 @@ const sending = {
 }
 
 localStorage.setItem('items', JSON.stringify(sending));
+localStorage.removeItem ('items');
     
 function Login(){
 try {
